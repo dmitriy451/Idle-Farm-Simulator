423aa38aa00a09f18db48d8320218f49070b4cbe
dmitriy451 <76558463+dmitriy451@users.noreply.github.com>
2022-08-03 20:21:46 +0300

    Added Animation
    Добавлена анимация для персонажа

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    void Movement()
+    {
+        
+    }
+}
